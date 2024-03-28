63bda100bfa3658a67441fc9c2a4df599a7af39b
IsmailHusun19 <ismailhusun2002>
2024-03-29 00:42:26 +0700

    main

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import FormInputData from './pages/InputData';
+import Footer from './component/Footer';
+import SignUp from './pages/SignUp';
+import Table from './component/Table';
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<FormInputData />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Table />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+  const currentURL = location.pathname;
+  const checking = !['/login', '/', '/signup', '/aboutus'].includes(currentURL);
+
+  return (
+    <>
+      {checking && <Navbar />}
+    </>
+  );
+}
+
+export default App;
